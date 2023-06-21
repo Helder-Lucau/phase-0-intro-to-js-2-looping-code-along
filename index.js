8160e2d2dc564901f34d0aa08e4ccd850568719a
Helder <helderlucau@gmail.com>
2023-06-21 20:48:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+//For loop
+function writeCards (cards, eventName) {
+    const newMessages = [];
+
+    for (let i = 0; i < cards.length; i++){
+        let message = (`Thank you, ${cards[i]}, for the wonderful ${eventName} gift!`);
+        newMessages.push(message);
+    }
+    return newMessages;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"))
+
+//While Loop
+function countDown(n){
+
+    while (n >= 0) {
+        console.log(n--);
+    }
+}
+countDown(10);
